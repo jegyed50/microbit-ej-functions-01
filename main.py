f17f58cb5ad5699d13f285cdc3f4c7dbc2fdd687
Egyed József <jegyed50@gmail.com>
2023-05-12 11:03:58 +0200

    Update pxt.json, main.blocks, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,59 @@
+def ShowText(text: str, ShowTimeOneLetter: number, PauseBetweenLetters: number):
+    global CurrentLetterIndex, CurrentLetter, CurrentLetterIndex2
+    CurrentLetterIndex = 0
+    while CurrentLetterIndex2 <= len(text):
+        CurrentLetter = text.substr(CurrentLetterIndex2, 1)
+        basic.show_string(CurrentLetter)
+        basic.pause(ShowTimeOneLetter)
+        basic.clear_screen()
+        basic.pause(PauseBetweenLetters)
+        CurrentLetterIndex2 += 1
+
+def on_button_pressed_a():
+    ShowText("abc", 200, 150)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    ShowNumber(123, 200, 150)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def ej_screensaver(switch_off_LED_max_brightness: number, fade_delay: number):
+    while True:
+        for x in range(5):
+            for y in range(5):
+                currentledbrightness = led.point_brightness(x, y)
+                if currentledbrightness > 0:
+                    for valtozofenyesseg in range(26):
+                        led.plot_brightness(x, y, currentledbrightness - valtozofenyesseg * 10)
+                        basic.pause(valtozofenyesseg * fade_delay)
+                    led.plot_brightness(x, y, 255)
+                    for valtozofenyesseg2 in range(26):
+                        led.plot_brightness(x, y, currentledbrightness - 0 * 10)
+                        basic.pause(valtozofenyesseg2 * fade_delay)
+                    led.plot_brightness(x, y, currentledbrightness)
+                else:
+                    valtozofenyesseg3 = 0
+                    while valtozofenyesseg3 <= switch_off_LED_max_brightness / 25:
+                        led.plot_brightness(x, y, valtozofenyesseg3 * 25)
+                        basic.pause(valtozofenyesseg3 * fade_delay * 10)
+                        valtozofenyesseg3 += 1
+                    led.plot_brightness(x, y, 0)
+                    basic.pause(fade_delay * 100)
+def ShowNumber(Number2: number, ShowTimeOneDigit: number, PauseBetweenDigits: number):
+    global NumberToText
+    NumberToText = convert_to_text(Number2)
+    ShowText(NumberToText, 200, 150)
+NumberToText = ""
+CurrentLetter = ""
+CurrentLetterIndex2 = 0
+CurrentLetterIndex = 0
+currentledbrightness2 = 0
+basic.show_icon(IconNames.HEART)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
+
+def on_in_background():
+    ej_screensaver(255, 2)
+control.in_background(on_in_background)
--- main.ts
+++ main.ts
@@ -1,33 +1,68 @@
-let CurrentLetterIndex = 0
-let CurrentLetter = ""
-let NumberToText = ""
-function ShowText (text: string, ShowTimeOneChar: number, PauseBetweenLetters: number) {
+function ShowText (text: string, ShowTimeOneLetter: number, PauseBetweenLetters: number) {
     CurrentLetterIndex = 0
-    for (let CurrentLetterIndex2 = 0; CurrentLetterIndex2 <= text.length; CurrentLetterIndex2++) {
+    while (CurrentLetterIndex2 <= text.length) {
         CurrentLetter = text.substr(CurrentLetterIndex2, 1)
         basic.showString(CurrentLetter)
-        basic.pause(ShowTimeOneChar)
+        basic.pause(ShowTimeOneLetter)
         basic.clearScreen()
         basic.pause(PauseBetweenLetters)
+        CurrentLetterIndex2 += 1
     }
 }
 input.onButtonPressed(Button.A, function () {
     ShowText("abc", 200, 150)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.AB, function () {
     ShowNumber(123, 200, 150)
 })
-function ShowNumber (Number2: number, ShowTimeOneDigit: number, PauseBetweenDigits: number) {
-    NumberToText = convertToText(Number2)
-    CurrentLetterIndex = 0
-    for (let CurrentLetterIndex3 = 0; CurrentLetterIndex3 <= NumberToText.length; CurrentLetterIndex3++) {
-        CurrentLetter = NumberToText.substr(CurrentLetterIndex3, 1)
-        basic.showString(CurrentLetter)
-        basic.pause(ShowTimeOneDigit)
-        basic.clearScreen()
-        basic.pause(PauseBetweenDigits)
+input.onButtonPressed(Button.B, function () {
+    ShowText(convertToText(123), 200, 150)
+})
+function ej_screensaver (switch_off_LED_max_brightness: number, fade_delay: number) {
+    while (true) {
+        for (let x = 0; x <= 4; x++) {
+            for (let y = 0; y <= 4; y++) {
+                currentledbrightness = led.pointBrightness(x, y)
+                if (currentledbrightness > 0) {
+                    for (let valtozofenyesseg = 0; valtozofenyesseg <= 25; valtozofenyesseg++) {
+                        led.plotBrightness(x, y, currentledbrightness - valtozofenyesseg * 10)
+                        basic.pause(valtozofenyesseg * fade_delay)
+                    }
+                    led.plotBrightness(x, y, 255)
+                    for (let valtozofenyesseg2 = 0; valtozofenyesseg2 <= 25; valtozofenyesseg2++) {
+                        led.plotBrightness(x, y, currentledbrightness - 0 * 10)
+                        basic.pause(valtozofenyesseg2 * fade_delay)
+                    }
+                    led.plotBrightness(x, y, currentledbrightness)
+                } else {
+                    valtozofenyesseg3 = 0
+                    while (valtozofenyesseg3 <= switch_off_LED_max_brightness / 25) {
+                        led.plotBrightness(x, y, valtozofenyesseg3 * 25)
+                        basic.pause(valtozofenyesseg3 * fade_delay * 10)
+                        valtozofenyesseg3 += 1
+                    }
+                    led.plotBrightness(x, y, 0)
+                    basic.pause(fade_delay * 100)
+                }
+            }
+        }
     }
 }
+function ShowNumber (Number2: number, ShowTimeOneDigit: number, PauseBetweenDigits: number) {
+    NumberToText = convertToText(Number2)
+    ShowText(NumberToText, 200, 150)
+}
+let NumberToText = ""
+let valtozofenyesseg3 = 0
+let currentledbrightness = 0
+let CurrentLetter = ""
+let CurrentLetterIndex2 = 0
+let CurrentLetterIndex = 0
+let currentledbrightness2 = 0
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
+	
+})
+control.inBackground(function () {
 	
 })
